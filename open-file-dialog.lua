04fe818fc703d8c5dcc3a6aabe1caeed8286bdbb
Ricardo Constantino <wiiaboo@gmail.com>
2016-03-09 17:12:00 +0000

    Temporarily turn ontop off while dialog is open

--- open-file-dialog.lua
+++ open-file-dialog.lua
@@ -7,6 +7,8 @@
 utils = require 'mp.utils'
 
 function open_file_dialog()
+	local was_ontop = mp.get_property_native("ontop")
+	if was_ontop then mp.set_property_native("ontop", false) end
 	local res = utils.subprocess({
 		args = {'powershell', '-NoProfile', '-Command', [[& {
 			Trap {
@@ -30,6 +32,7 @@ function open_file_dialog()
 		}]]},
 		cancellable = false,
 	})
+	if was_ontop then mp.set_property_native("ontop", true) end
 	if (res.status ~= 0) then return end
 
 	local first_file = true
