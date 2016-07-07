afff96d785c9a95acdf87041f021c367c099c470
vaviary <mail@viktor.im>
2016-07-07 10:37:21 +0200

    Initial commit of open-url-dialog

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
-open-file-dialog.lua
+open-url-dialog.lua
 ====================
 
-**open-file-dialog.lua** is a script for mpv that can launch a regular Windows
-file open dialog from a key binding (default: Ctrl+O.)
+**open-url-dialog.lua** is a script for mpv that can launch a text dialog to allow for easy opening of URLs, such as YouTube links (default: Ctrl+Shift+O.) It is a fork of [open-file-dialog](https://github.com/rossy/mpv-open-file-dialog). :sparkles:
 
-![screenshot](https://rossy.github.io/mpv-open-file-dialog/screen.png)
-
-This script requires PowerShell. It should work on all Windows versions that
-mpv supports. Tested in Windows 8.1 and 10.
+This script requires PowerShell. It should work on all Windows versions that mpv supports. 
 
 ![CC0](https://licensebuttons.net/p/zero/1.0/80x15.png)
--- open-url-dialog.lua
+++ open-url-dialog.lua
@@ -6,7 +6,7 @@
 
 utils = require 'mp.utils'
 
-function open_file_dialog()
+function open_url_dialog()
 	local was_ontop = mp.get_property_native("ontop")
 	if was_ontop then mp.set_property_native("ontop", false) end
 	local res = utils.subprocess({
@@ -15,31 +15,23 @@ function open_file_dialog()
 				Write-Error -ErrorRecord $_
 				Exit 1
 			}
-			Add-Type -AssemblyName PresentationFramework
+			Add-Type -AssemblyName Microsoft.VisualBasic
 
 			$u8 = [System.Text.Encoding]::UTF8
 			$out = [Console]::OpenStandardOutput()
 
-			$ofd = New-Object -TypeName Microsoft.Win32.OpenFileDialog
-			$ofd.Multiselect = $true
+      $urlname = [Microsoft.VisualBasic.Interaction]::InputBox("Enter URL:", "Open a URL", "http[s]://")
+      $u8urlname = $u8.GetBytes("$urlname`n")
+      $out.Write($u8urlname, 0, $u8urlname.Length)
 
-			If ($ofd.ShowDialog() -eq $true) {
-				ForEach ($filename in $ofd.FileNames) {
-					$u8filename = $u8.GetBytes("$filename`n")
-					$out.Write($u8filename, 0, $u8filename.Length)
-				}
-			}
 		}]]},
 		cancellable = false,
 	})
 	if was_ontop then mp.set_property_native("ontop", true) end
 	if (res.status ~= 0) then return end
 
-	local first_file = true
-	for filename in string.gmatch(res.stdout, '[^\n]+') do
-		mp.commandv('loadfile', filename, first_file and 'replace' or 'append')
-		first_file = false
-	end
+  mp.commandv('loadfile', res.stdout)
 end
 
-mp.add_key_binding('ctrl+o', 'open-file-dialog', open_file_dialog)
+mp.add_key_binding('ctrl+O', 'open-url-dialog', open_url_dialog)
+
